5c22e900698aaf500f7cb7d3e9affe081472a7af
mrbloom <mrbloom@ukr.net>
2021-04-12 12:31:47 +0300

    test aws 4

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const proccess = require("process")
 
 const app = express()
-const port = proccess.env.port || 3000
+const port = proccess.env.PORT || 3000
+console.log(`PORT = ${port}`)
 
 app.listen(port,()=>{
     console.log("ddddd")
